d699fa81b82c2d02d2be75f1d51aaec193d2a7d6
Alivia Nytko <anytko@vols.utk.edu>
2024-05-19 13:16:02 -0400

    Fixing deployment issues

--- trading_project/trading_project/settings.py
+++ trading_project/trading_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-cfdr#-j1@!d&qx1x$wuy3@a-zh)vb-^!^aek-1!+t3ib$&12^0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,9 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 # Default primary key field type
